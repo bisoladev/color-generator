d6cdb480280de1586e960b15056458f6eec09f96
Bisola Onaolapo <bisolaonaolapo81@gmail.com>
2022-08-30 13:30:51 +0100

    setError to false in the try block

--- src/SingleColor.js
+++ src/SingleColor.js
@@ -3,12 +3,17 @@ import React, { useState, useEffect } from "react";
 const SingleColor = ({ color, index }) => {
   const [alert, setAlert] = useState(false);
 
-  const {weight, rgb, hex} = color;
+  const { weight, rgb, hex } = color;
   const bcg = rgb.join(",");
+
+  const hexValue = `#${hex}`
   return (
-    <article className={`color ${index > 10 && 'color-light'}`} style={{ backgroundColor: `rgb(${bcg})` }}>
+    <article
+      className={`color ${index > 10 && "color-light"}`}
+      style={{ backgroundColor: `rgb(${bcg})` }}
+    >
       <p className="percent-value">{weight}%</p>
-      <p className="color-value">{`#${hex}`}</p>
+      <p className="color-value">{hexValue}</p>
     </article>
   );
 };
