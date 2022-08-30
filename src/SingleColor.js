d80ed3c258708e08ddb487d20324ea73ee2c1ff1
Bisola Onaolapo <bisolaonaolapo81@gmail.com>
2022-08-30 13:07:36 +0100

    Replaced spread operator with props assignment

--- src/SingleColor.js
+++ src/SingleColor.js
@@ -1,12 +1,15 @@
 import React, { useState, useEffect } from "react";
 import rgbToHex from "./utils";
 
-const SingleColor = ({ rgb, weight, index }) => {
+const SingleColor = ({ color, index }) => {
   const [alert, setAlert] = useState(false);
+
+  const {weight, rgb, hex} = color;
   const bcg = rgb.join(",");
   return (
     <article className={`color`} style={{ backgroundColor: `rgb(${bcg})` }}>
-      {""}
+      <p className="percent-value">{weight}%</p>
+      <p className="color-value">{`#${hex}`}</p>
     </article>
   );
 };
